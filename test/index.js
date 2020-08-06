299622f920866781f81c898251e1a9d666a3287f
Nguyen Tuan Linh <tuanlinh.nguyen@dsquare.com.vn>
2020-08-06 09:38:31 +0700

    v1.0.0

---
+++ test/index.js
@@ -0,0 +1,12 @@
+import mocha from 'mocha'
+import chai from 'chai'
+
+const describe = mocha.describe
+const it = mocha.it
+const expect = chai.expect
+chai.should()
+
+describe('node-js-module-starter', function () {
+    describe('unit tests', function () {
+    })
+})
