06f7fcd2a2521d54d310702c36318a803ea61310
Nguyen Tuan Linh <linhntaim@users.noreply.github.com>
2023-10-05 13:35:57 +0700

    + preload + .npmignore

---
+++ config-expand.js
@@ -0,0 +1,14 @@
+(function () {
+    const dotenvExpand = require('dotenv-expand').expand
+    const dotenvConversion = require('./dist').convert
+
+    const env = require('dotenv').config(
+        Object.assign(
+            {},
+            require('dotenv/lib/env-options'),
+            require('dotenv/lib/cli-options')(process.argv),
+        ),
+    )
+
+    return dotenvConversion(dotenvExpand(env))
+})()
---
+++ config.js
@@ -0,0 +1,13 @@
+(function () {
+    const dotenvConversion = require('./dist').convert
+
+    const env = require('dotenv').config(
+        Object.assign(
+            {},
+            require('dotenv/lib/env-options'),
+            require('dotenv/lib/cli-options')(process.argv),
+        ),
+    )
+
+    return dotenvConversion(env)
+})()
